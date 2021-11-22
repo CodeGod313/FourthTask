33c01af14a0d27ac9565e046d7c5a49f694c2776
Ales <codegod313@gmail.com>
2021-11-22 03:25:03 +0300

    Regex brackets insertion fail

---
+++ src/test/java/edu/epam/fourthtask/parser/ExpressionHandlerTest.java
@@ -0,0 +1,26 @@
+package edu.epam.fourthtask.parser;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ExpressionHandlerTest {
+
+    ExpressionHandler expressionHandler;
+    @BeforeAll
+    public void setUp(){
+        expressionHandler = new ExpressionHandler();
+    }
+
+
+    @Test
+    void handle() {
+        Double actual = expressionHandler.handle("(7^5|1&2<<(2|5>>2&71))|1200");
+        Double expected = 0.;
+        Assertions.assertEquals(expected, actual);
+    }
+}
